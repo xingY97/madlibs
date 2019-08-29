f80a93f7f37efe6bdd902441493eb57b21843432
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-08-29 14:36:28 -0700

    Madlibs second commit

--- Madlibs.py
+++ Madlibs.py
@@ -3,8 +3,7 @@
 adjective = input ("Enter a adjective ")
 nationality = input ("Enter a nationality")
 named = input ("Enter a name")
+noun = input ("Entre a noun")
 
 
-print("Pizza was invented by a  " + adjective)
-print(nationality)
-print("chef named" + named)
+print("Pizza was invented by a  " + adjective + nationality + " chef named " + named + ". to make a pizza, you need to take a lump of " + noun )
