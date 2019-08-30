19cac69450dbb78a8c7318dd8935c2f6739f36bc
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-08-30 13:53:50 -0700

    fourth commit

--- Madlibs.py
+++ Madlibs.py
@@ -6,6 +6,11 @@ named = input ("Enter a name")
 noun = input ("Entre a noun")
 second_adjective = input ("Enter a second adjective")
 second_noun = input ("Enter a second noun")
+third_adjective = input ("Enter a thir adjective")
+forth_adjective = input ("Enter a forth adjejctive")
+Plural_noun = input ("Enter a Plural_noun")
+third_noun = input ("Enter a third_noun")
+number = input ("Enter a number")
 
 
-print("Pizza was invented by a  " + adjective + nationality + " chef named " + named + ". to make a pizza, you need to take a lump of" + noun + " gitand make a thin, round" + second_adjective + second_noun + "."  )
+print("Pizza was invented by a  " + adjective + nationality + " chef named " + named + ". to make a pizza, you need to take a lump of" + noun + " gitand make a thin, round" + second_adjective + second_noun + "." + "Then you cover it with " + third_adjective + "Sauce " + forth_adjective + " cheese, and fresh chopped " + Plural_noun + "NEst you have to bake it in a very hot " + third_noun + "When it is done, cut it into " + number )
