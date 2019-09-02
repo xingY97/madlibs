b7e8ea0166f459171acfe3f37dafcb9e215bcad9
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-09-01 20:00:45 -0700

    adjusting code

--- Madlibs.py
+++ Madlibs.py
@@ -1,20 +1,18 @@
 
+def mad_libs ():
+    story = "pizza was invented by a {adjective} {country} chef named {name}"
+    list = {}
+    list["adjective"] = input ("Enter a adjejctive: ")
+    list["country"] = input ("Enter a country")
+    list["name"] = input ("Enter a name")
 
-adjective = input ("Enter a adjective ")
-nationality = input ("Enter a nationality")
-named = input ("Enter a name")
-noun = input ("Entre a noun")
-second_adjective = input ("Enter a second adjective")
-second_noun = input ("Enter a second noun")
-third_adjective = input ("Enter a thir adjective")
-forth_adjective = input ("Enter a forth adjejctive")
-Plural_noun = input ("Enter a Plural_noun")
-third_noun = input ("Enter a third_noun")
-number = input ("Enter a number")
-shape = input ("Enter a shape")
-food = input ("Enter a food")
-second_food = input ("Enter second_food")
-second_number = input ("Enter a second_number")
+    print (story)
 
 
-print("Pizza was invented by a  " + adjective + nationality + " chef named " + named + ". to make a pizza, you need to take a lump of" + noun + " gitand make a thin, round" + second_adjective + second_noun + "." + "Then you cover it with " + third_adjective + "Sauce " + forth_adjective + " cheese, and fresh chopped " + Plural_noun + "Next you have to bake it in a very hot " + third_noun + "When it is done, cut it into " + number + shape + "." + "Some kids like " + food + "Pizza the best, but my favorite is the " + second_food + "pizza." + "If I could, I would eat pizza " + second_number + "times a day!"  )
+def story():
+    template = "Didn't you hear? %(noun)-%(verb)sing %(noun)s are the new %(color)s"
+    dictionary = {}
+    dictionary["noun"] = raw_input("Enter a noun: ")
+    dictionary["verb"] = raw_input("Enter a -ing verb: ")
+    dictionary["color"] = raw_input("Enter a color: ")
+    print (template % dictionary)
