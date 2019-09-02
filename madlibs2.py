8b877c11fd939672c70a97d53126f18154c13396
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-09-02 11:02:47 -0700

    improved code2

--- madlibs2.py
+++ madlibs2.py
@@ -1,12 +1,19 @@
-def getKeys(formatString):
+
+def main (): #https://www.pinterest.com/pin/375839531387234463 story found here.
+    story = """ Pizza was invented by a {adjective} {nationality} chef named {name}. To make a pizza, you need to take a lump of {noun}, and make a thin, round {adjective}{noun}. Then you cover it with {adjective} sauce, {adjective} cheese, and fresh chopped {plural_noun}. Next you have to bake it in a very hot {noun}. When it is done, cut it into {number} {shapes}. some kids like {food} pizza. If I could, I would eat pizza {number} times a day. """
+
+    tellStory(story)
+    print("come back Soon!")
+def getKeys(user_input):
     keylist = list ()
     end = 0
-    words = formatString.count('{')
+    words = user_input.count('{')
     for i in range (words):
-        start = formatString.find('{', end) + 1
-        end = formatString.find('}', start)
-        key = formatString[start : end]
+        start = user_input.find('{', end) + 1
+        end = user_input.find('}', start)
+        key = user_input[start : end]
         keylist.append(key)
+
     return set(keylist) #set removes duplicates
 
 def addPicks(cue, dictionary): # "Enter a specific example for {name}: "
@@ -14,6 +21,7 @@ def addPicks(cue, dictionary): # "Enter a specific example for {name}: "
     prompt = user_input.format(name=cue)
     response = input(prompt)
     dictionary[cue] = response
+
 
 def getUserPicks(cues):
      userPicks = dict()
@@ -26,11 +34,6 @@ def tellStory(storyFormat):
     userPicks = getUserPicks(cues)
     story = storyFormat.format(**userPicks)
     print(story)
-def main (): #https://www.pinterest.com/pin/375839531387234463 story found here.
-    story = """ Pizza was invented by a {adjective} {nationality} chef named {name}. To make a pizza, you need to take a lump of {noun}, and make a thin, round {adjective}{noun}. Then you cover it with {adjective} sauce, {adjective} cheese, and fresh chopped {plural_noun}. Next you have to bake it in a very hot {noun}. When it is done, cut it into {number} {shapeds}. some kids like {food} pizza. If I could, I would eat pizza {number} times a day. """
-
-    tellStory(story)
-    print("come back Soon!")
 
 main()
 
