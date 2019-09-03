9dca5e70e6f292a77e1e8ac90d5027e2d45b5ed5
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-09-02 17:59:11 -0700

    added color

--- madlibs2.py
+++ madlibs2.py
@@ -1,6 +1,7 @@
 
+
 def main (): #https://www.pinterest.com/pin/375839531387234463 story found here.
-    story = """ Pizza was invented by a {adjective} {nationality} chef named {name}. To make a pizza, you need to take a lump of {noun}, and make a thin, round {adjective}{noun}. Then you cover it with {adjective} sauce, {adjective} cheese, and fresh chopped {plural_noun}. Next you have to bake it in a very hot {noun}. When it is done, cut it into {number} {shapes}. some kids like {food} pizza. If I could, I would eat pizza {number} times a day. """
+    story = """ \033[42m Pizza was invented by a \033[0;32m{adjective} \033[0;34m {nationality} chef named {name}. To make a pizza, you need to take a lump of \033[0;37m {noun}, and make a thin, round \033[4;33m{adjective} \033[4;33m{noun}. Then you cover it with \033[0;31m{adjective} sauce, \033[0;35m {adjective} cheese, and fresh chopped \033[1;31m{plural_noun}. Next you have to bake it in a very hot {noun}. When it is done, cut it into {number} {shapes}. some kids like {food} pizza. If I could, I would eat pizza {number} times a day. """
 
     tellStory(story)
     print("come back Soon!")
@@ -22,18 +23,17 @@ def addPicks(cue, dictionary): # "Enter a specific example for {name}: "
     response = input(prompt)
     dictionary[cue] = response
 
-
 def getUserPicks(cues):
-     userPicks = dict()
-     for cue in cues:
-         addPicks(cue,userPicks)
-     return userPicks
-
+    userPicks = dict()
+    for cue in cues:
+        addPicks(cue,userPicks)
+    return userPicks
 def tellStory(storyFormat):
     cues = getKeys(storyFormat)
     userPicks = getUserPicks(cues)
     story = storyFormat.format(**userPicks)
-    print(story)
+    print(story) 
+
 
 main()
 
